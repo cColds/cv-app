a4989c849ac3ecf4245ad532a0a61b0810bb7742
cColds <103373668+cColds@users.noreply.github.com>
2023-05-21 22:45:25 -0500

    Add set and edit personalInfo

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -14,16 +14,66 @@ export default class Resume extends Component {
         item: { fullName: "", jobTitle: "", phone: "", email: "" },
         form: { fullName: "", jobTitle: "", phone: "", email: "" },
       },
+      displayForm: { personalInfo: false },
     };
+
+    this.setPersonalInfoForm = this.setPersonalInfoForm.bind(this);
+    this.setPersonalInfoItem = this.setPersonalInfoItem.bind(this);
+    this.toggleForm = this.toggleForm.bind(this);
+  }
+
+  setPersonalInfoForm(e) {
+    const { personalInfo } = this.state;
+    const { form } = personalInfo;
+    const updateForm = {
+      ...form,
+      [e.target.name]: e.target.value,
+    };
+
+    this.setState({
+      personalInfo: {
+        ...personalInfo,
+        form: updateForm,
+      },
+    });
+  }
+
+  setPersonalInfoItem(e) {
+    e.preventDefault();
+    const { personalInfo } = this.state;
+    const { form } = personalInfo;
+    const updateItem = {
+      ...form,
+    };
+
+    this.setState({
+      personalInfo: {
+        ...personalInfo,
+        item: updateItem,
+      },
+    });
+  }
+
+  toggleForm(e) {
+    const { displayForm } = this.state;
+    const isFormOpen = displayForm[e.currentTarget.name];
+    this.setState({
+      displayForm: { ...displayForm, [e.currentTarget.name]: !isFormOpen },
+    });
   }
 
   render() {
-    const { personalInfo } = this.state;
-    const { fullName } = personalInfo.form;
+    const { personalInfo, displayForm } = this.state;
 
     return (
       <div className="resume">
-        <PersonalInfo fullName={fullName} />
+        <PersonalInfo
+          setPersonalInfoForm={this.setPersonalInfoForm}
+          setPersonalInfoItem={this.setPersonalInfoItem}
+          toggleForm={this.toggleForm}
+          personalInfo={personalInfo}
+          isFormOpen={displayForm.personalInfo}
+        />
         <Experience />
         <Education />
         <Projects />
